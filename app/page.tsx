c0212a0c15bae8d9601a3b51f31a1a701e26d0a0
Madhushree Gupta <madhushreegupta@madhushrees-mbp.home>
2024-12-20 10:42:02 +0000

    Setup Light and Dark mode, Navbar and shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => (
-	<h1 className='text-3xl text-violet-700 font-black'>
-		Welcome to next js world
-	</h1>
+  <>
+    <h1 className="text-3xl text-violet-700 font-black">
+      Welcome to next js world
+    </h1>
+  </>
 );
 
 export default Home;
